70d9b99261311535afc6e4a554bcf1f6f398de63
codewalker <codewalker@hardkernel.com>
2017-10-20 15:33:14 +0900

    ODROID-N1: Update build script.
    Change-Id: Ic20ac9c42ef327d3594334f9a7bc28fb6c101533

--- build.sh
+++ build.sh
@@ -1,35 +1,19 @@
 #!/bin/bash
+export ARCH=arm64
+export ARCHV=aarch64
+export CROSS_COMPILE=aarch64-linux-android-
+export PATH=/opt/toolchains/aarch64-linux-android-4.9/bin:$PATH
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
+source build/envsetup.sh
+lunch odroidn1-eng
 
 CPU_JOB_NUM=$(grep processor /proc/cpuinfo | awk '{field=$NF};END{print field+1}')
 ROOT_DIR=$(pwd)
 
-echo 'build u-boot'
-pushd u-boot
-make odroidn1_defconfig
 make -j$CPU_JOB_NUM
-popd
 
-echo 'build kernel'
-pushd kernel
-make -j$CPU_JOB_NUM odroidn1_defconfig
-make -j$CPU_JOB_NUM rk3399-odroidn1-rev0.img
-echo 'build kernel module'
-make -j$CPU_JOB_NUM
-popd
-
-MODULES_DIR=out/target/product/odroidn1/system/lib/modules
-
-echo 'cp ko files'
-mkdir -p $MODULES_DIR
-find hardware/wifi kernel -name *.ko | xargs -i cp {} $MODULES_DIR
-
-echo 'build android'
-source build/envsetup.sh
-lunch odroidn1-eng
-make -j8
-
-echo 'make images'
 ./mkimage.sh
 
-echo 'cp sd_fusing.sh'
-cp u-boot/sd_fuse/* rockdev/Image-odroidn1/
+make -j$CPU_JOB_NUM updatepackage selfinstall
