2a9e18814b0febffb516def5999af3173493caeb
codewalker <codewalker@hardkernel.com>
2017-10-30 14:43:17 +0900

    ODROID-N1: Update build.sh.
    Change-Id: I9c2a213ad404d7e7371144a1a8468ea61bef9d28

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 export ARCH=arm64
 export ARCHV=aarch64
 export CROSS_COMPILE=aarch64-linux-android-
-export PATH=/opt/toolchains/aarch64-linux-android-4.9/bin:$PATH
+export PATH=/opt/toolchains/aarch64-linux-android-4.9/bin:/opt/toolchains/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin:$PATH
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
 
@@ -13,7 +13,5 @@ CPU_JOB_NUM=$(grep processor /proc/cpuinfo | awk '{field=$NF};END{print field+1}
 ROOT_DIR=$(pwd)
 
 make -j$CPU_JOB_NUM
-
-./mkimage.sh
 
 make -j$CPU_JOB_NUM updatepackage selfinstall
