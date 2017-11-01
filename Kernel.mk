ed516bb80bbbf00aae3e5277177c8e112140b85d
Luke Go <luke.go@hardkernel.com>
2017-11-01 14:56:57 +0900

    ODROID-N1: u-boot: Changed u-boot make scripts.
    Change-Id: I2e8b265109fbc66486df9329f82d3e5b2bed576f

--- Kernel.mk
+++ Kernel.mk
@@ -5,7 +5,6 @@ INSTALLED_KERNEL_TARGET := $(PRODUCT_OUT)/kernel
 KERNEL_ARCH := arm64
 KERNEL_DEFCONFIG := odroidn1_defconfig
 PREFIX_CROSS_COMPILE := aarch64-linux-android-
-UBOOT_CROSS_COMPILE := aarch64-linux-gnu-
 
 KERNEL_ROOTDIR := kernel
 KERNEL_CONFIG := $(KERNEL_ROOTDIR)/.config
@@ -29,8 +28,9 @@ $(KERNEL_IMAGE): $(KERNEL_CONFIG) $(KERNEL_MODULES_OUT)
 UBOOT_ARCHV := aarch64
 UBOOT_DEFCONFIG := odroidn1_defconfig
 UBOOT_ROOTDIR := u-boot
-UBOOT_IMAGE := $(UBOOT_ROOTDIR)/sd_fuse/u-boot.bin
+UBOOT_IMAGE := $(UBOOT_ROOTDIR)/u-boot.bin
 UBOOT_CONFIG := $(UBOOT_ROOTDIR)/.config
+UBOOT_CROSS_COMPILE := aarch64-linux-gnu-
 
 $(UBOOT_CONFIG):
 	$(MAKE) -C $(UBOOT_ROOTDIR) ARCHV=$(UBOOT_ARCHV) \
@@ -39,16 +39,6 @@ $(UBOOT_CONFIG):
 $(UBOOT_IMAGE): $(UBOOT_CONFIG)
 	$(MAKE) -C $(UBOOT_ROOTDIR) ARCHV=$(UBOOT_ARCHV) \
 		CROSS_COMPILE=$(UBOOT_CROSS_COMPILE)
-	@pushd $(UBOOT_ROOTDIR) && \
-	tools/rk_tools/bin/loaderimage --pack --uboot u-boot-dtb.bin uboot.img && \
-	tools/mkimage -n rk3399 -T rksd -d tools/rk_tools/bin/rk33/rk3399_ddr_800MHz_v1.08.bin idbloader.img && \
-	cat tools/rk_tools/bin/rk33/rk3399_miniloader_v1.06.bin >> idbloader.img && \
-	cp tools/rk_tools/bin/rk33/rk3399_loader_v1.08.106.bin sd_fuse && \
-	tools/rk_tools/bin/trust_merger tools/rk_tools/trust.ini && \
-	cp idbloader.img sd_fuse && \
-	cp uboot.img sd_fuse && \
-	cp trust.img sd_fuse && \
-	popd
 
 RAMDISK_MKIMG := $(PRODUCT_OUT)/ramdisk_mkimg.img
 
