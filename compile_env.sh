8817c2b42d7155627c2542312189f2c16b8f409c
codewalker <codewalker@hardkernel.com>
2017-11-02 10:37:21 +0900

    ODROID-N1: Add path for u-boot compiler.
    Change-Id: If5660c5547c0842b2c854c63f6d29f20b6db0f67

--- compile_env.sh
+++ compile_env.sh
@@ -2,6 +2,6 @@
 export ARCH=arm64
 export ARCHV=aarch64
 export CROSS_COMPILE=aarch64-linux-android-
-export PATH=/opt/toolchains/aarch64-linux-android-4.9/bin:$PATH
+export PATH=/opt/toolchains/aarch64-linux-android-4.9/bin:/opt/toolchains/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin:$PATH
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
